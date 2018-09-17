e7a394a1ebd821042fad3485da9206d7547a2f31
최용호 <yongho1037@gmail.com>
2018-09-18 00:03:14 +0900

    3장 예제 추가

---
+++ jsx/example_3-4.jsx
@@ -0,0 +1,16 @@
+let helloWorldReactElement = <h1>Hello world!</h1>
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>
+                {helloWorldReactElement}
+                {helloWorldReactElement}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <HelloWorld/>,
+    document.getElementById('content')
+)
